d750560f1c6f39adb2ecdd019b3559b11177454d
San-Tri <stri80603@gmail.com>
2024-05-03 16:42:56 -0500

    archivoCss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <style>
-        /* Estilos generales */
-        body, html {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-            height: 100%;
-            /* justify-content: space-between; */
-        }
-
-        /* Estilos del encabezado */
-        header {
-            background-color: #333;
-            color: #fff;
-            padding: 20px 0;
-            width: 100%;
-            top: 0;
-            align: icon;
-        }
-
-        header h1 {
-            margin: 0;
-        }
-
-        nav ul {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-        }
-
-        nav ul li {
-            display: inline;
-            margin-right: 20px;
-        }
-
-        nav ul li a {
-            color: #fff;
-            text-decoration: none;
-        }
-
-        .navegar {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 0 20px;
-        }
-        
-        /* Estilos del pie de página */
-        footer {
-            bottom: 0;
-            width: 100%;
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 20px 0;
-        }
-        
-    </style>
 </head>
 <body>
 <header>
