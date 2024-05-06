638634bb73a2018d9e51a737aa0107e655f2aa15
San-Tri <stri80603@gmail.com>
2024-05-06 12:31:44 -0500

    nuevoDiseñoPrincipal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,11 +23,83 @@
     </div>
 </header>
 
+<div class="contenedor">
+    <div class="card">
+        <img class="fondo" src="https://i.pinimg.com/736x/82/be/59/82be59e2305a0841dc023ffa919f30de.jpg" alt="">
+        <div class="contenido-card">
+            <div class="perfil-imagen">
+                <i class="bi bi-bag"></i>
+            </div>
+            <h3 class="titulo">PELUCHES</h3>
+        </div>
+        <div class="telon"></div>
+    </div>
+
+    <div class="card">
+        <img class="fondo" src="https://i.pinimg.com/564x/75/b6/69/75b669a747c2a7aab390a5dda99635e5.jpg" alt="">
+        <div class="contenido-card">
+            <div class="perfil-imagen">
+                <i class="bi bi-bag"></i>
+            </div>
+            <h3 class="titulo">FIGURAS</h3>
+        </div>
+        <div class="telon"></div>
+    </div>
+
+    <div class="card">
+        <img class="fondo" src="https://i.pinimg.com/564x/62/da/f3/62daf317ec3c6fb8e096ff9ce83243de.jpg" alt="">
+        <div class="contenido-card">
+            <div class="perfil-imagen">
+                <i class="bi bi-bag"></i>
+            </div>
+            <h3 class="titulo">ACCESORIOS</h3>
+        </div>
+        <div class="telon"></div>
+    </div>
+
+    <div class="card">
+        <img class="fondo" src="https://i.pinimg.com/564x/54/0d/30/540d30e80abd4e5a4478e1c6897c390d.jpg" alt="">
+        <div class="contenido-card">
+            <div class="perfil-imagen">
+                <i class="bi bi-bag"></i>
+            </div>
+            <h3 class="titulo">ROPA</h3>
+        </div>
+        <div class="telon"></div>
+    </div>
+
+    <div class="card">
+        <img class="fondo" src="https://i.pinimg.com/564x/8a/70/df/8a70dfa83232d67bf5b20ec14ccced72.jpg" alt="">
+        <div class="contenido-card">
+            <div class="perfil-imagen">
+                <i class="bi bi-bag"></i>
+            </div>
+            <h3 class="titulo">MANGAS</h3>
+        </div>
+        <div class="telon"></div>
+    </div>
+</div>
 
 <footer>
     <div class="container">
         <p>Derechos de Autor &copy; 2024 Portal de Servicios</p>
     </div>
 </footer>
+
+<script type="text/javascript">
+const cardsContainer = document.querySelector(".contenedor");
+
+cardsContainer.addEventListener("click", (e) => {
+  const target = e.target.closest(".card");
+
+  if (!target) return;
+
+  cardsContainer.querySelectorAll(".card").forEach((card) => {
+    card.classList.remove("active");
+  });
+
+  target.classList.add("active");
+});
+</script>
 </body>
 </html>
